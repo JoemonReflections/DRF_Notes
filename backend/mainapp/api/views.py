36bd5787f747436c1b6d35fb13563c26604ecc67
JoemonV-Reflections <105621339+JoemonReflections@users.noreply.github.com>
2023-09-18 07:40:21 +0000

    DRF Code Added

---
+++ backend/mainapp/api/views.py
@@ -0,0 +1,5 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+@api_view(["GET"])
+def home(request):
+    return Response({"data":"Notes API Started"})
